dd603f55d240f00c5ba37434f35ffabb033f2938
Dimitry Machado Marinho <dimitrym.marinho@gmail.com>
2020-07-30 20:40:10 -0300

    Commit aula 03

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 import Button from '../../../components/Button';
+import FormField from '../../../components/FormField';
 
 const CadastroCategoria = () => {
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    }
+    const [categorias, setCategorias] = useState([])
+    const [values, setValues] = useState(valoresIniciais);
+
+
+
+    const setValue = (chave, valor) => {
+        setValues({
+            ...values,
+            [chave]: valor, //nome: 'valor'
+        })
+    };
+
+    function funcHandler(infoEvent) {
+
+        setValue(
+            infoEvent.target.getAttribute('name'),
+            infoEvent.target.value,
+        );
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>            
+            <h1>Cadastro de Categoria : {values.nome}</h1>
+            <form onSubmit={function handleSubmit(infoEvent) {
+                infoEvent.preventDefault();
+
+                setCategorias([
+                    ...categorias,
+                    values
+                ]);
+                setValues(valoresIniciais);
+            }}>
+                <div>
+
+                    <FormField
+                        label='Nome da Categoria: '
+                        type='text'
+                        name='nome'
+                        value={values.nome}
+                        onChange={funcHandler}
+                    />
+
+                    <div>
+
+
+                        <FormField
+                            label="Descrição:"
+                            type='textArea'
+                            value={values.descricao}
+                            onChange={funcHandler}
+                        />
+
+                    </div>
+                    <FormField
+                        label="Cor: "
+                        type='color'
+                        value={values.cor}
+                        onChange={funcHandler}
+                    />
+                </div>
+
+                <button>
+                    Cadastrar
+                </button>
+
+            </form>
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                            {categoria.nome}
+                        </li>
+                    )
+                })}
+            </ul>
             <Button as={Link} className='ButtonLink' to='/'>
                 Home
             </Button>
 
-        </PageDefault>
+        </PageDefault >
     );
 }
 
