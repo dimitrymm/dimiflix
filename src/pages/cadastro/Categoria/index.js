cc4e4aef3402ed886d609f08042b467b3766248e
Dimitry Machado Marinho <dimitrym.marinho@gmail.com>
2020-07-31 06:50:07 -0300

    Update

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable react/no-array-index-key */
+
 /* eslint-disable linebreak-style */
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
@@ -7,90 +9,90 @@ import Button from '../../../components/Button';
 import FormField from '../../../components/FormField';
 
 const CadastroCategoria = () => {
-  const valoresIniciais = {
-    nome: '',
-    descricao: '',
-    cor: '',
-  };
-  const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    };
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
 
-  const setValue = (chave, valor) => {
-    setValues({
-      ...values,
-      [chave]: valor, // nome: 'valor'
-    });
-  };
+    const setValue = (chave, valor) => {
+        setValues({
+            ...values,
+            [chave]: valor, // nome: 'valor'
+        });
+    };
 
-  function funcHandler(infoEvent) {
-    setValue(
-      infoEvent.target.getAttribute('name'),
-      infoEvent.target.value,
-    );
-  }
+    function funcHandler(infoEvent) {
+        setValue(
+            infoEvent.target.getAttribute('name'),
+            infoEvent.target.value,
+        );
+    }
 
-  return (
-    <PageDefault>
-      <h1>
-        Cadastro de Categoria :
+    return (
+        <PageDefault>
+            <h1>
+                Cadastro de Categoria :
         {values.nome}
-      </h1>
-      <form onSubmit={function handleSubmit(infoEvent) {
-        infoEvent.preventDefault();
+            </h1>
+            <form onSubmit={function handleSubmit(infoEvent) {
+                infoEvent.preventDefault();
 
-        setCategorias([
-          ...categorias,
-          values,
-        ]);
-        setValues(valoresIniciais);
-      }}
-      >
-        <div>
+                setCategorias([
+                    ...categorias,
+                    values,
+                ]);
+                setValues(valoresIniciais);
+            }}
+            >
+                <div>
 
-          <FormField
-            label="Nome da Categoria: "
-            type="text"
-            name="nome"
-            value={values.nome}
-            onChange={funcHandler}
-          />
+                    <FormField
+                        label="Nome da Categoria: "
+                        type="text"
+                        name="nome"
+                        value={values.nome}
+                        onChange={funcHandler}
+                    />
 
-          <div>
+                    <div>
 
-            <FormField
-              label="Descrição:"
-              type="textArea"
-              value={values.descricao}
-              onChange={funcHandler}
-            />
+                        <FormField
+                            label="Descrição:"
+                            type="textArea"
+                            value={values.descricao}
+                            onChange={funcHandler}
+                        />
 
-          </div>
-          <FormField
-            label="Cor: "
-            type="color"
-            value={values.cor}
-            onChange={funcHandler}
-          />
-        </div>
+                    </div>
+                    <FormField
+                        label="Cor: "
+                        type="color"
+                        value={values.cor}
+                        onChange={funcHandler}
+                    />
+                </div>
 
-        <Button>
-          Cadastrar
+                <Button>
+                    Cadastrar
         </Button>
 
-      </form>
-      <ul>
-        {categorias.map((categoria, indice) => (
-          <li key={`${categoria}${indice}`}>
-            {categoria.nome}
-          </li>
-        ))}
-      </ul>
-      <Button as={Link} className="ButtonLink" to="/">
-        Home
+            </form>
+            <ul>
+                {categorias.map((categoria, indice) => (
+                    <li key={`${categoria}${indice}`}>
+                        {categoria.nome}
+                    </li>
+                ))}
+            </ul>
+            <Button as={Link} className="ButtonLink" to="/">
+                Home
       </Button>
 
-    </PageDefault>
-  );
+        </PageDefault>
+    );
 };
 
 export default CadastroCategoria;
